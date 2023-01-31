348b9b470bf73122bff23f95ca24f16ed0ed9253
shiam <amamun.shiam@tetonelectronics.com>
2023-01-31 09:56:37 +0600

    updated

--- gf_demo/settings.py
+++ gf_demo/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -122,6 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+MODELS = os.path.join(BASE_DIR, 'models')
 
 STATIC_URL = 'static/'
 STATIC_ROOT = "/static/"
